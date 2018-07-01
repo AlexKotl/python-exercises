735ad9b6d5ef7d460420809911ff759030bc5a2a
Alex <slicer256@gmail.com>
2018-07-01 22:51:50 +0300

    chapter 9

--- chapter 9/1.py
+++ chapter 9/1.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from pprint import pprint
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=".", static_url_path='', template_folder='.')
 
 @app.route("/")
 def index():
-    return "Hi"
+    #return app.send_static_file('template.html')
+    return render_template('template.html', content = request.args.get("content"), color = request.args.get("color"))
 
-app.run(port=5000, debug=True)
+app.run(port=5001, debug=True)
