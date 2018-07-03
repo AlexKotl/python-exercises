5cfe0b1c3b91748953f750b4ece78e386a031a84
Alex <slicer256@gmail.com>
2018-07-04 00:43:07 +0300

    render refactor

--- polls/poll/views.py
+++ polls/poll/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
 
-    return HttpResponse(loader.get_template('poll/index.html').render({
+    return render(request, 'poll/index.html', {
         'latest_questions': latest_questions,
-    }, request))
+    })    
 
 def details(request, question_id):
     return HttpResponse("Question: %s" % question_id)
