919f83b71af19af98641065a258d6a0d9915f327
Alex <slicer256@gmail.com>
2018-07-04 00:26:06 +0300

    basic views

--- polls/poll/views.py
+++ polls/poll/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello Django")
+
+def details(request, question_id):
+    return HttpResponse("Question: %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("Results for: %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote for: %s" % question_id)
