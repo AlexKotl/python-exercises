664e2c1b89a48e40efcae7dd87f6aa453a6af64d
Alex <slicer256@gmail.com>
2018-07-04 22:28:46 +0300

    refactored views

--- polls/poll/views.py
+++ polls/poll/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.views import generic
 from .models import Question
 
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_questions'
+
+    def get_queryset(self):
+        ''' Return last five questions '''
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/details.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'poll/details.html', {
+            question: question,
+            error_message: "Choice not existed",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+
+# OLD STYLE VIEWS, NOT USED
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
 
@@ -23,17 +55,3 @@ def details(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'poll/results.html', {'question': question})
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except(KeyError, Choice.DoesNotExist):
-        return render(request, 'poll/details.html', {
-            question: question,
-            error_message: "Choice not existed",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
